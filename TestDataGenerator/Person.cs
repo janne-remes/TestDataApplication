8efd3c9c16edfc6f259d23dfb38f36657733a7b9
janne-remes <janneremes@outlook.com>
2020-04-09 13:54:46 +0300

    Add project files.

---
+++ TestDataGenerator/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDataGenerator
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public static List<string> FirstNameMales = new List<string>()
+        {
+            "Pekka",
+            "Arto",
+            "Tauno",
+            "Mikko",
+            "Sakari",
+            "Paavo",
+            "Risto",
+            "Olavi"
+        };
+    }
+}
---
+++ TestDataGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace TestDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person dudeOne = new Person("Pekka", "Paavola", 54);
+
+            Console.WriteLine($"{dudeOne.firstName} {dudeOne.lastName}, {dudeOne.age} vuotta");
+
+        }
+    }
+}
